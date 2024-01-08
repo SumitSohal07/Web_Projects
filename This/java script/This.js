e5ba392946d50b9797cf1b814e74505be74924a5
SumitSohal07 <145088567+SumitSohal07@users.noreply.github.com>
2024-01-07 23:29:41 -0500

    Add files via upload
    Here I have used "This" event to callback with the eventListener .when the user clicks on the text that is shown on the screen it's color will going to change to blue.

---
+++ This/java script/This.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector("button");
+let p = document.querySelector("p");
+let h1 = document.querySelector("h1");
+let h3 = document.querySelector("h3");
+
+function changecolor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "blue";
+}
+
+btn.addEventListener("click", changecolor);
+p.addEventListener("click", changecolor);
+h1.addEventListener("click", changecolor);
+h3.addEventListener("click", changecolor);
